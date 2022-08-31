b0882bac56e803b251873fa5d03e90abc17d9dff
rahilsama <rahil.sama@ucalgary.ca>
2022-08-31 15:33:54 -0600

    Arrow Function

--- script03.js
+++ script03.js
@@ -23,6 +23,8 @@ console.log(fruit_processor(5,0));
 
 //Declaration vs Expression
 
+
+//Function Declaration 
 function calculate_age_1(birth_year) {
     const age = 2022 - birth_year;
     return age;
@@ -30,9 +32,28 @@ function calculate_age_1(birth_year) {
 
 console.log(calculate_age_1(1999));
 
+
+//Function Expression 
 const calculate_age_2 = function(birth_year){
     return 2022-birth_year;
 }
 
 console.log(calculate_age_2(1999));
 
+
+//Arrow Function 
+//Added in ES6
+//Shorter and faster to write 
+const calculate_age_3 = birth_year => 2022 - birth_year;
+
+console.log(calculate_age_3(1999));
+
+const years_until_retirement = (birth_year, first_name) => {
+    const age = 2022 - birth_year;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${first_name} retires in ${retirement} years.`;
+}
+
+
+console.log(years_until_retirement(1999, 'Rahil'));
