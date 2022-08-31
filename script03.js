c74ca4f5b9432bfed07bb525f3f48d9f30f10c04
rahilsama <rahil.sama@ucalgary.ca>
2022-08-31 15:06:28 -0600

    Functions

--- script03.js
+++ script03.js
@@ -5,3 +5,34 @@ function logger() {
 
 
 logger();
+logger();
+logger();
+logger();
+
+function fruit_processor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+
+
+
+console.log(fruit_processor(5,0));
+
+
+//Declaration vs Expression
+
+function calculate_age_1(birth_year) {
+    const age = 2022 - birth_year;
+    return age;
+}
+
+console.log(calculate_age_1(1999));
+
+const calculate_age_2 = function(birth_year){
+    return 2022-birth_year;
+}
+
+console.log(calculate_age_2(1999));
+
