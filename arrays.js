3cf4c6ec142651713f141a4afb34aa33609d727d
Rahil Sama <rahil.sama@ucalgary.ca>
2022-09-07 22:20:05 -0600

    Arrays in JS

---
+++ arrays.js
@@ -0,0 +1,32 @@
+//Arrays
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const years = new Array(1999, 2015, 2017, 2022);
+console.log(years);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+
+friends[2] = "Jay";
+console.log(friends);
+
+
+const person = ["Rahil", "Sama", 2022-1999, friends];
+
+console.log(person);
+
+//Excercise
+const calcAge = function (birth_year) {
+    return 2022 - birth_year;
+}
+
+const year = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(year[0]);
+const age2 = calcAge(year[1]);
+const age3 = calcAge(year[year.length - 1]);
+
+console.log(age1, age2, age3);
