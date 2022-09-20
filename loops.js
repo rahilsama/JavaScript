0d99ecdb2be1db7df083f3173a7af439a26dfb42
Rahil Sama <rahil.sama@ucalgary.ca>
2022-09-20 09:34:14 -0600

    loops

---
+++ loops.js
@@ -0,0 +1,25 @@
+
+
+
+console.log("Lifing weights repitition 1 🏋️‍♀️");
+console.log("Lifing weights repitition 2 🏋️‍♀️");
+console.log("Lifing weights repitition 3 🏋️‍♀️");
+console.log("Lifing weights repitition 4 🏋️‍♀️");
+console.log("Lifing weights repitition 5 🏋️‍♀️");
+console.log("Lifing weights repitition 6 🏋️‍♀️");
+console.log("Lifing weights repitition 7 🏋️‍♀️");
+console.log("Lifing weights repitition 8 🏋️‍♀️");
+console.log("Lifing weights repitition 9 🏋️‍♀️");
+console.log("Lifing weights repitition 10 🏋️‍♀️");
+
+for(let rep = 1; rep <= 10; rep++){
+    console.log(`Lifting weights repitition ${rep} 🏋️‍♀️`);
+}
+
+
+const nameArray = ["Rahil", "Sama", 22, "Developer", ["Mustan", "Ravi", "Insha"]];
+
+for(let i = 0; i < nameArray.length; i++){
+    console.log(nameArray[i], typeof nameArray[i]);
+}
+
