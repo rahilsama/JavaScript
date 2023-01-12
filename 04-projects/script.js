8fb749adb2c181d52801b1c7532ee4b6ae970fa6
Rahil Sama <rahil.sama@ucalgary.ca>
2023-01-11 17:44:10 -0700

    just debugging tut

--- 04-projects/script.js
+++ 04-projects/script.js
@@ -1,1 +1,12 @@
-'use strict';
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: prompt("Degrees celsius"),
+  };
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
